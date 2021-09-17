3f365e5fb84ca08c59f22b4cc364f4334868225d
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-09-18 05:51:38 +0600

    Added spinnner on loading images and added feature to handle not found image

--- app.js
+++ app.js
@@ -14,19 +14,25 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 // show images
 const showImages = images => {
-   imagesArea.style.display = 'block';
+   //Added feature to handle not found image
    gallery.innerHTML = '';
-   // show gallery title
-   galleryHeader.style.display = 'flex';
-   images.forEach(image => {
-      let div = document.createElement('div');
-      div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-      div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-      gallery.appendChild(div);
-   });
+   if (images.length !== 0) {
+      imagesArea.style.display = 'block';
+      // show gallery title
+      galleryHeader.style.display = 'flex';
+      images.forEach(image => {
+         let div = document.createElement('div');
+         div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+         div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+         gallery.appendChild(div);
+      });
+   }
+   toggleSpinner();
+   gallery.innerHTML += `<h1 class="text-danger"> No image found based on your query. Please try again! </h1>`;
 };
 
 const getImages = query => {
+   toggleSpinner();
    fetch(
       `https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`
    )
@@ -39,7 +45,7 @@ let slideIndex = 0;
 
 const selectItem = (event, img) => {
    let element = event.target;
-   element.classList.toggle('added'); //image select toggler
+   element.classList.toggle('added'); //image select toggler style
 
    let item = sliders.indexOf(img);
    if (item === -1) {
@@ -148,3 +154,9 @@ document.getElementById('search').addEventListener('keyup', event => {
       searchBtn.click();
    }
 });
+
+//Toggle spinner function
+const toggleSpinner = () => {
+   const spinner = document.getElementById('spinner-loading');
+   spinner.classList.toggle('d-none');
+};
